512a5caddbefaf790ccc37a1633e6cd7f2ec70bc
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-01-09 17:58:57 +0100

    Edit api

--- src/api/users/RegisterUserAPI.ts
+++ src/api/users/RegisterUserAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const registerUser = async (username: string, password: string) => {
-	const url = `${import.meta.env.VITE_API_KEY}/register`;
+	const url = `${import.meta.env.VITE_API_KEY}register`;
 	try {
 		const response = await axios.post(url, {
 			username,
